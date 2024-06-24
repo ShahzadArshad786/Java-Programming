e2f874358a7ccf212b387c1d453704a8b23e58ce
Your Name <you@example.com>
2024-06-24 18:15:33 +0500

    "First Commit"

---
+++ Array Programs/LargestNumberInArray.java
@@ -0,0 +1,42 @@
+        /*
+         *  Write a JAVA Program that inpus n number of elements and finds the Largest 
+         *  Number from the Array. 
+         */
+
+         import java.util.Scanner; 
+         public class LargestNumberInArray {
+             public static void main(String[] args) {
+              
+                 Scanner sc = new Scanner(System.in); 
+                 
+                 int size , max ; 
+                 
+                 System.out.print("Enter Size of Array : ");
+                 size = sc.nextInt();
+                 
+                 int[] arr = new int [size]; 
+                 
+                 System.out.println("\nEnter "+ size +" Array of Elements : \n");
+ 
+                 for (int i = 0 ; i < arr.length ; i++)
+                 {
+                     System.out.print("Enter Element at Index Arr["+i+"] : ");
+                     arr[i] = sc.nextInt(); 
+                 }
+     
+                 max = arr[0] ; 
+     
+                 System.out.print("\nArray Elements are  : " );
+                 for (int i = 0 ; i < arr.length ; i++)
+                 {
+                     System.out.print(arr[i] + " ");
+                     if(arr[i] > max )
+                     {
+                         max = arr[i];
+                     }
+                 }
+                 System.out.println("\nLargest  Number is  : " + max);
+                 sc.close();
+             }
+         }
+ 
---
+++ Array Programs/SumOfArrayElements.java
@@ -0,0 +1,40 @@
+    /*
+        Write a JAVA Program that inputs N Number of Array Elements and Finds the
+        Sum of Array Elements. 
+    */
+    
+    import java.util.Scanner ; 
+    public class SumOfArrayElements {
+        public static void main(String[] args) {
+            
+            Scanner sc = new Scanner(System.in); 
+        
+            int size , sum = 0 ; 
+            
+            System.out.print("Enter Size of Array : ");
+            size = sc.nextInt();
+            
+            int[] arr = new int [size]; 
+            
+            int length = arr.length; 
+
+            System.out.println("\nEnter "+ size +" Array of Elements : \n");
+
+            for (int i = 0 ; i < length ; i++)
+            {
+                System.out.print("Enter Element at Index Arr["+i+"] : ");
+                arr[i] = sc.nextInt(); 
+            }
+        
+            System.out.print("\nArray Elements  : " );
+            for(int i = 0 ; i < length ; i ++)
+            {
+                System.out.print(arr[i] + " ");
+                sum = sum + arr[i]; 
+            }
+            
+            System.out.print("\nSum of Array is : " + sum); 
+            sc.close();
+        }
+    }
+
