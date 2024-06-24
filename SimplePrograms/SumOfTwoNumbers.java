7391344ebafab3fd263eb13fd3198fa8f7ea38d4
Your Name <you@example.com>
2024-06-24 06:47:21 +0500

    "First Commit"

--- SimplePrograms/SumOfTwoNumbers.java
+++ SimplePrograms/SumOfTwoNumbers.java
@@ -2,10 +2,9 @@
     import java.util.Scanner;
 
     public class SumOfTwoNumbers 
-    {
+    {        
         public static void main(String[] args) 
         {
-        
             Scanner sc = new Scanner(System.in);
             
             int num1 , num2 , sum = 0 ; 
