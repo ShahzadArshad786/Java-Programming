6c8eac315a3c60b1faa46bb90a68bffb66fa18f5
Your Name <you@example.com>
2024-06-24 17:15:57 +0500

    "First Commit"

---
+++ Array Programs/SmallestNumberInArray.java
@@ -0,0 +1,41 @@
+        /*
+         *  Write a JAVA Program that inpus n number of elements and find the smallest 
+         *  Number from the Array. 
+         */
+
+        import java.util.Scanner;
+        public class SmallestNumberInArray {
+        public static void main(String[] args) {
+            
+            Scanner sc = new Scanner(System.in); 
+                
+            int size , min ; 
+            
+            System.out.print("Enter Size of Array : ");
+            size = sc.nextInt();
+            
+            int[] arr = new int [size]; 
+            
+            System.out.println("\nEnter "+ size +" Array of Elements : \n");
+            for (int i = 0 ; i < arr.length ; i++)
+            {
+                System.out.print("Enter Element at Index Arr["+i+"] : ");
+                arr[i] = sc.nextInt(); 
+            }
+
+            min = arr[0] ; 
+
+            System.out.print("\nArray Elements are  : " );
+            for (int i = 0 ; i < arr.length ; i++)
+            {
+                System.out.print(arr[i] + " ");
+                if(arr[i] < min )
+                {
+                    min = arr[i];
+                }
+            }
+            System.out.println("\nSmallest Number is  : " + min);
+            sc.close();
+        }       
+    }
+
---
+++ Array Programs/SumOfTwoArrays.java
@@ -0,0 +1,67 @@
+ /*
+            Write a JAVA Program that inputs two Arrays with equal size and find the
+            SUM of Both Arrays. 
+         */
+       
+        import java.util.*;
+        public class SumOfTwoArrays {
+            public static void main(String[] args) {
+                
+                Scanner sc = new Scanner(System.in); 
+            
+                int n1 , n2 ; 
+                
+                System.out.print("Enter Size of 1st Array : ");
+                n1 = sc.nextInt();
+                
+                int[] arr1 = new int [n1] ; 
+                
+                System.out.print("\nEnter "+ arr1.length + " Elements in 1st Array\n");
+                for (int i = 0 ; i < arr1.length ; i++)
+                {
+                    System.out.print("Enter Element at Index Arr["+i+"] : ");
+                    arr1[i] = sc.nextInt(); 
+                }
+            
+                System.out.print("\nEnter Size of 2nd Array : ");
+                n2 = sc.nextInt();
+                
+                int[] arr2 = new int [n2] ; 
+                
+                if(arr2.length < arr1.length || arr2.length > arr1.length)
+                {
+                    System.out.print("\nSize of Both Arrays Should be Equal\n");
+                }
+                else 
+                {
+                    System.out.print("\nEnter "+ arr2.length + " Elements in 2nd Array\n");
+            
+                    for (int i = 0 ; i < arr2.length ; i++)
+                    {
+                        System.out.print("Enter Element at Index Arr["+i+"] : ");
+                        arr2[i] = sc.nextInt(); 
+                    } 
+                }
+            
+                System.out.print("\n\n1st Array is       : ");
+                for(int i = 0 ; i < arr1.length ; i++)
+                {
+                System.out.print( arr1[i] + "  ");
+                }   
+            
+                System.out.print("\n\n2nd Array is       : ");
+                for(int i = 0 ; i < arr2.length ; i++)
+                {
+                System.out.print( arr2[i] + "  ");
+                }   
+            
+                System.out.print("\n\nResultant Array is : "); 
+                for(int i = 0 ; i < arr1.length ; i++)
+                {
+                System.out.print((arr1[i] + arr2[i]) + "  ");
+                }   
+                sc.close();
+            }
+        }
+
+
