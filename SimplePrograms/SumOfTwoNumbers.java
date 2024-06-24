3ccef1166eece5d5cd8c2f06100b67f21ce7fcaf
Your Name <you@example.com>
2024-06-24 06:46:09 +0500

    "First Commit"

--- SimplePrograms/SumOfTwoNumbers.java
+++ SimplePrograms/SumOfTwoNumbers.java
@@ -1,27 +1,29 @@
-package SimplePrograms;
-import java.util.Scanner;
+    package SimplePrograms;
+    import java.util.Scanner;
 
-public class SumOfTwoNumbers {
-    public static void main(String[] args) {
+    public class SumOfTwoNumbers 
+    {
+        public static void main(String[] args) 
+        {
         
-    Scanner sc = new Scanner(System.in);
-    
-    int num1 , num2 , sum = 0 ; 
+            Scanner sc = new Scanner(System.in);
+            
+            int num1 , num2 , sum = 0 ; 
 
-    // Input First Number
-    System.out.print("Enter Value For 1st Number : ");
-    num1 = sc.nextInt(); 
+            // Input First Number
+            System.out.print("Enter Value For 1st Number : ");
+            num1 = sc.nextInt(); 
 
-    // Input Second Number
-    System.out.print("Enter Value For 2nd Number : ");
-    num2 = sc.nextInt(); 
+            // Input Second Number
+            System.out.print("Enter Value For 2nd Number : ");
+            num2 = sc.nextInt(); 
 
-    // Caluculate Sum Of Two Numbers
-    sum = num1 + num2 ; 
+            // Caluculate Sum Of Two Numbers
+            sum = num1 + num2 ; 
 
-    // Display Sum Of Two Numbers
-    System.out.println("\nSum of " + num1 + " and " + num2 + " is : " + sum);
+            // Display Sum Of Two Numbers
+            System.out.println("\nSum of " + num1 + " and " + num2 + " is : " + sum);
 
-    sc.close(); 
+            sc.close(); 
+        }
     }
-}
